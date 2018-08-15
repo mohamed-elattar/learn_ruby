7d9d747e3fb166b4d8d4ffc0d2fdad07a718442d
mohamed elattar <maaelattar@gmail.com>
2018-08-15 17:48:34 +0200

    Solve 02_calculator

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,36 @@
-#write your code here
+def add(number_1, number_2)
+  number_1 + number_2
+end
+
+def subtract(number_1, number_2)
+  number_1 - number_2
+end
+
+def sum(arr)
+  sum = 0
+  arr.each do |n|
+    sum += n
+  end
+  sum
+end
+
+def multiply(*numbers)
+  result = 1
+  numbers.each do |n|
+    result *= n
+  end
+  result
+end
+
+def power(number, power)
+  number**power
+end
+
+def factorial(number)
+  result = 1
+  return result if number <= 1
+  (1..number).each do |n|
+    result *= n
+  end
+  result
+end
