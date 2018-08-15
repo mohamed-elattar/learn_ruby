41deda36646624cd0ff23c67ebfd8756c1d1bc2e
mohamed elattar <maaelattar@gmail.com>
2018-08-15 17:49:52 +0200

    Solve 04_pig_latin

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,18 @@
-#write your code here
+def translate(str)
+  vowels = %w[a e i o qu]
+  str_arr = str.split(' ')
+  str_arr.each do |i|
+    i.each_char do |c|
+      next unless vowels.include?(c)
+      index = i.index(c)
+      if index.zero?
+        i.replace("#{i}ay")
+      else
+        sub = i[0..(index - 1)]
+        i.replace("#{i[index..str.length] + sub}ay")
+      end
+      break
+    end
+  end
+  str_arr.join(' ')
+end
