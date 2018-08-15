821be6b71acf507c4575c3370233543d0537bab6
mohamed elattar <maaelattar@gmail.com>
2018-08-15 17:50:42 +0200

    Solve 06_timer

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,21 @@
-class Timer
-  #write your code here
-end
+class Timer
+  attr_accessor :seconds
+  def initialize
+    @seconds = 0
+  end
+
+  def time_string
+    second = @seconds % 60
+    minute = ((@seconds - second) % (60 * 60)) / 60
+    hour = (@seconds - minute - second) / (60 * 60)
+
+    time_string = padded("#{hour}:#{minute}:#{second}")
+    time_string
+  end
+
+  def padded(str)
+    str_arr = str.split(':')
+    str_arr.map { |i| i.length == 2 ? i : i.replace("0#{i}") }
+    str_arr.join(':')
+  end
+end
