eec5bf41969351f122928f89fd176d2cd5551883
mohamed elattar <maaelattar@gmail.com>
2018-08-15 17:49:29 +0200

    Solve 03_simon_says

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,29 @@
-#write your code here
+def echo(str)
+  str
+end
+
+def shout(str)
+  str.upcase
+end
+
+def repeat(str, number = 2)
+  ("#{str} " * number).strip!
+end
+
+def start_of_word(str, number = 1)
+  str[0..(number - 1)]
+end
+
+def first_word(str)
+  str.split(' ')[0]
+end
+
+def titleize(str)
+  little_words = %w[on the and over in]
+  str_arr = str.split(' ')
+  str_arr[0].capitalize!
+  str_arr.map do |i|
+    little_words.include?(i) ? i : i.capitalize!
+  end
+  str_arr.join(' ')
+end
