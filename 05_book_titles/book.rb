8ca039e94da04690a0bdc7e3df0dc955a567746d
mohamed elattar <maaelattar@gmail.com>
2018-08-15 17:50:18 +0200

    Solve 05_book_titles

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,10 @@
-class Book
-# write your code here
-end
+class Book
+  attr_writer :title
+  def title
+    little_words = %w[in on the a over a an and or of]
+    str_arr = @title.strip.split(' ')
+    str_arr[0].capitalize!
+    str_arr.map { |i| little_words.include?(i) ? i : i.capitalize! }
+    @title = str_arr.join(' ')
+  end
+end
